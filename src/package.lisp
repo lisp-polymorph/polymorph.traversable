bfd4085dbcfa04f5870242963605ab22640c0e92
Andrey Sokolov <aun.sokolov@gmail.com>
2021-05-18 15:14:49 +0300

    Find returns 2 values

--- src/package.lisp
+++ src/package.lisp
@@ -3,12 +3,13 @@
 (defpackage #:polymorph.traversable
   (:use #:cl #:adhoc-polymorphic-functions #:alexandria #:polymorph.utility)
   (:local-nicknames (:cm :sandalphon.compiler-macro)
-                    (:mop :closer-mop))
+                    (:mop :closer-mop)
+                    (:ie :introspect-environment))
   (:shadow #:find #:find-if
            #:position #:position-if
            #:count #:count-if)
   (:shadowing-import-from #:polymorph.maths #:=)
-  (:shadowing-import-from #:polymorph.access #:at #:row-major-at)
+  (:shadowing-import-from #:polymorph.access #:at #:row-major-at #:size)
   (:export #:find #:find-if
          #:position #:position-if
          #:count #:count-if))
--- src/polymorph.traversable.lisp
+++ src/polymorph.traversable.lisp
@@ -8,93 +8,90 @@
 
 
 
-(declaim (inline %inline-finder))
-(defun %inline-finder (item container &key from-end start end key test)
-  (if from-end
-        (if end
-            (if key
-                (if test
-                    (cl:find item container :from-end from-end
-                                            :start start :end end
-                                            :key key :test test)
-                    (cl:find item container :from-end from-end
-                                            :start start :end end
-                                            :key key))
-                (if test
-                    (cl:find item container :from-end from-end
-                                            :start start :end end
-                                            :test test)
-                    (cl:find item container :from-end from-end
-                                            :start start :end end)))
-            (if key
-                (if test
-                    (cl:find item container :from-end from-end
-                                            :start start
-                                            :key key :test test)
-                    (cl:find item container :from-end from-end
-                                            :start start
-                                            :key key))
-                (if test
-                    (cl:find item container :from-end from-end
-                                            :start start
-                                            :test test)
-                    (cl:find item container :from-end from-end
-                                            :start start))))
-        (if end
-            (if key
-                (if test
-                    (cl:find item container :start start :end end
-                                            :key key :test test)
-                    (cl:find item container :start start :end end
-                                            :key key))
-                (if test
-                    (cl:find item container :start start :end end
-                                            :test test)
-                    (cl:find item container :start start :end end)))
-            (if key
-                (if test
-                    (cl:find item container :start start
-                                            :key key :test test)
-                    (cl:find item container :start start
-                                            :key key))
-                (if test
-                    (cl:find item container :start start
-                                            :test test)
-                    (cl:find item container :start start))))))
-
-
-
-
-
 (defpolymorph (find :inline t) ((item t) (container array) &key ((from-end boolean) nil)
-                                ((start ind) 0) ((end (maybe ind)) nil)
+                                ((start ind) 0) ((end ind) (size container))
                                 ((key (maybe function)) nil) ((test (maybe function)) nil))
     t
   (if (= 1 (array-rank container))
-      (%inline-finder item container :from-end from-end :start start :end end
-                                     :key key :test test)
-      (if (and (= start 0) (not (or end from-end)))
+      (if from-end
+          (if key
+              (if test
+                  (loop :for i :of-type ind :from (1- end) :downto start
+                        :for elt := (funcall key (at container i))
+                        :when (funcall test item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil)))
+                  (loop :for i :of-type ind :from (1- end) :downto start
+                        :for elt := (funcall key (at container i))
+                        :when (= item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil))))
+              (if test
+                  (loop :for i :of-type ind :from (1- end) :downto start
+                        :for elt := (at container i)
+                        :when (funcall test item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil)))
+                  (loop :for i :of-type ind :from (1- end) :downto start
+                        :for elt := (at container i)
+                        :when (= item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil)))))
+          (if key
+              (if test
+                  (loop :for i :of-type ind :from start :below end
+                        :for elt := (funcall key (at container i))
+                        :when (funcall test item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil)))
+                  (loop :for i :of-type ind :from start :below end
+                        :for elt := (funcall key (at container i))
+                        :when (= item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil))))
+              (if test
+                  (loop :for i :of-type ind :from start :below end
+                        :for elt := (at container i)
+                        :when (funcall test item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil)))
+                  (loop :for i :of-type ind :from start :below end
+                        :for elt := (at container i)
+                        :when (= item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil))))))
+
+      (if (and (= start 0) (= end (size container)) (not from-end))
           (if key
               (if test
                   (loop :for i :of-type ind :from 0 :below (array-total-size container)
                         :for cand := (funcall key (row-major-at container i))
                         :when (funcall test cand item)
-                          :do (return cand))
+                          :do (return (values cand t))
+                        :finally (return (values nil nil)))
                   (loop :for i :of-type ind :from 0 :below (array-total-size container)
                         :for cand := (funcall key (row-major-at container i))
                         :when (= cand item)
-                          :do (return cand)))
+                          :do (return (values cand t))
+                        :finally (return (values nil nil))))
               (if test
                   (loop :for i :of-type ind :from 0 :below (array-total-size container)
                         :for cand := (row-major-at container i)
                         :when (funcall test cand item)
-                          :do (return cand))
+                          :do (return (values cand t))
+                        :finally (return (values nil nil)))
                   (loop :for i :of-type ind :from 0 :below (array-total-size container)
                         :for cand := (row-major-at container i)
                         :when (= cand item)
-                          :do (return cand))))
+                          :do (return (values cand t))
+                        :finally (return (values nil nil)))))
           (error 'simple-error :format-control
                  "Start, end or from-end arguments cannot be provided for multidimensional arrays"))))
+
+
+
+
+
 
 
 (defpolymorph-compiler-macro find (t array &key (:from-end boolean) (:start ind) (:end (maybe ind))
@@ -105,12 +102,73 @@
     `(the (maybe ,elt-type) ,form)))
 
 
+
 (defpolymorph (find :inline t) ((item t) (container list) &key ((from-end boolean) nil)
                                 ((start ind) 0) ((end (maybe ind)) nil)
                                 ((key (maybe function)) nil) ((test (maybe function)) nil))
     t
-  (%inline-finder item container :from-end from-end :start start :end end
-                                 :key key :test test))
+  (if from-end
+      (find item (reverse container) :start start :end end :key key :test test)
+      (if end
+          (if key
+              (if test
+                  (loop :for cursor := (nthcdr start container) :then (rest cursor)
+                        :while cursor
+                        :repeat (- end start)
+                        :for elt := (funcall key (first cursor))
+                        :when (funcall test item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil)))
+                  (loop :for cursor := (nthcdr start container) :then (rest cursor)
+                        :while cursor
+                        :repeat (- end start)
+                        :for elt := (funcall key (first cursor))
+                        :when (= item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil))))
+              (if test
+                  (loop :for cursor := (nthcdr start container) :then (rest cursor)
+                        :while cursor
+                        :repeat (- end start)
+                        :for elt := (first cursor)
+                        :when (funcall test item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil)))
+                  (loop :for cursor := (nthcdr start container) :then (rest cursor)
+                        :while cursor
+                        :repeat (- end start)
+                        :for elt := (first cursor)
+                        :when (= item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil)))))
+          (if key
+              (if test
+                  (loop :for cursor := (nthcdr start container) :then (rest cursor)
+                        :while cursor
+                        :for elt := (funcall key (first cursor))
+                        :when (funcall test item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil)))
+                  (loop :for cursor := (nthcdr start container) :then (rest cursor)
+                        :while cursor
+                        :for elt := (funcall key (first cursor))
+                        :when (= item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil))))
+              (if test
+                  (loop :for cursor := (nthcdr start container) :then (rest cursor)
+                        :while cursor
+                        :for elt := (first cursor)
+                        :when (funcall test item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil)))
+                  (loop :for cursor := (nthcdr start container) :then (rest cursor)
+                        :while cursor
+                        :for elt := (first cursor)
+                        :when (= item elt)
+                          :do (return (values elt t))
+                        :finally (return (values nil nil))))))))
+
 
 
 (defpolymorph (find :inline t) ((item t) (container hash-table) &key ((from-end boolean) nil)
@@ -124,32 +182,44 @@
                           :using (hash-value v)
                         :for pair := (cons k v)
                         :when (funcall test item pair)
-                          :do (return pair)))
-            ((eql :key) (values (gethash item container))) ;;;TODO test is ignored here
+                          :do (return (values pair t))
+                        :finally (return (values nil nil))))
+            ((eql :key) (loop :for k :being :the :hash-key :in container
+                              :when (funcall test item k)
+                                :do (return (values k t))
+                              :finally (return (values nil nil)))) ;;;TODO sometimes gethash is good
             ((eql :value) (loop :for v :being :the :hash-value :in container
-                             :when (funcall test item v)
-                               :do (return v)))
+                                :when (funcall test item v)
+                                  :do (return (values v t))
+                                :finally (return (values nil nil))))
             (otherwise
              (loop :for k :being :the :hash-keys :in container
-               :using (hash-value v)
-              :for pair := (cons k v)
-              :when (funcall test item (funcall key pair))
-                :do (return pair))))
+                     :using (hash-value v)
+                   :for pair := (cons k v)
+                   :when (funcall test item (funcall key pair))
+                     :do (return (values pair t))
+                   :finally (return (values nil nil)))))
           (typecase key
             (null (loop :for k :being :the :hash-keys :in container
                           :using (hash-value v)
                         :for pair := (cons k v)
                         :when (= item pair)
-                          :do (return pair)))
-            ((eql :key) (values (gethash item container)))
+                          :do (return (values pair t))
+                        :finally (return (values nil nil))))
+            ((eql :key) (loop :for k :being :the :hash-key :in container
+                              :when (= item k)
+                                :do (return (values k t))
+                              :finally (return (values nil nil))))
             ((eql :value) (loop :for v :being :the :hash-value :in container
-                             :when (= item v)
-                               :do (return v)))
+                                :when (= item v)
+                                  :do (return (values v t))
+                                :finally (return (values nil nil))))
             (otherwise
              (loop :for k :being :the :hash-keys :in container
-               :using (hash-value v)
-              :for pair := (cons k v)
-              :when (= item (funcall key pair))
-                :do (return pair)))))
+                     :using (hash-value v)
+                   :for pair := (cons k v)
+                   :when (= item (funcall key pair))
+                     :do (return (values pair t))
+                   :finally (return (values nil nil))))))
       (error 'simple-error :format-control
              "Start, end or from-end arguments cannot be provided for hash-tables")))
